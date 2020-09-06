c19eef91204574ecaa90f25d7dd0f9e8a2a10acb
Michael Bykov <m.bykov@gmail.com>
2020-09-07 01:15:15 +0300

    i

--- src/index.js
+++ src/index.js
@@ -56,6 +56,9 @@ export async function md2json(bpath) {
       doc.level = level
       md = md.replace(/#/g, '').trim()
     }
+    else if (/^\d/.test(md)) {
+      md = '&nbsp;' + md
+    }
     doc.md = md
     docs.push(doc)
   }
