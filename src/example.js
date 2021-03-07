a9c9cd88be24ce3b3c73cf52f56e7fe3a3261ceb
Michael Bykov <m.bykov@gmail.com>
2021-03-07 15:42:44 +0300

    Readme

--- src/example.js
+++ src/example.js
@@ -7,32 +7,15 @@ import { md2json } from "./index";
 
 let bpath = '../test/fixtures/'
 let fn = 'ok-test-eng.md'
-fn = 'markdown-test-eng.md'
-fn = 'markdown-test-list.md'
-fn = 'xxxxx'
-// fn = 'Robinson-Crusoe.eng.md'
-// fn = 'book_1.gr.aristotle.md'
-// fn = 'Harry-Potter-and-the-Order-of-the-Phoenix.md'
-// fn = 'Le-Petit-Prince.rus.md'
 
 bpath = path.resolve(__dirname, bpath, fn)
 log('RUN BPATH:', bpath)
-let lang = 'eng'
 
 async function start(bpath) {
   let {descr, docs, imgs}  = await md2json(bpath)
   log('_descr:', descr)
-  if (!docs) return
-  let footnotes = docs.filter(doc=> doc.footnote)
-  let endnotes = docs.filter(doc=> doc._id)
-  let refs = docs.filter(doc=> doc.refnote)
-  let headers = docs.filter(doc=> doc.level > -1)
 
-  log('_docs:', docs)
-  log('_footnotes:', footnotes.length)
-  log('_endnotes:', endnotes.length)
-  log('_refs:', refs.length)
-  log('_headers:', headers.length)
+  log('_docs:', docs.length)
 }
 
 start(bpath)
